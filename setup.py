4a8c5f1f0c69adc574a2643a6bc02521c5bdaa2a
Dan Moldovan <mdan@google.com>
2017-11-07 15:50:11 -0500

    Rev build number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('LICENSE') as f:
 
 setup(
     name='tangent',
-    version='0.1.3',
+    version='0.1.4',
     description=('Automatic differentiation using source code transformation '
                  'for Python'),
     long_description=readme,
