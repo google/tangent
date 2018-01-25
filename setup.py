972ff9c5b70eb18446640fb68d2fe2d05b4559bb
Dan Moldovan <mdan@google.com>
2018-01-25 11:43:19 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('LICENSE') as f:
 
 setup(
     name='tangent',
-    version='0.1.7',
+    version='0.1.10',
     description=('Automatic differentiation using source code transformation '
                  'for Python'),
     long_description=readme,
